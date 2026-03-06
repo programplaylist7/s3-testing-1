4c8380299e1dac9589742f716741c1b422adfd8a
ProgramPlaylist7 <programplaylist7@gmail.com>
2026-03-06 23:39:09 +0530

    handler added and yml file changed

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
-import { connectDB } from "./config/connectDB.js";
+import mongoose from "mongoose";
+import serverless from "serverless-http";
 dotenv.config();
-const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
-connectDB();
+// MongoDB connection caching (IMPORTANT for Lambda)
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  const db = await mongoose.connect(process.env.MONGO_URI);
+  isConnected = db.connections[0].readyState;
+  console.log("MongoDB Connected");
+};
 
 app.get("/", (req, res) => {
     res.json({
@@ -14,6 +23,12 @@ app.get("/", (req, res) => {
     })
 });
 
-app.listen(PORT, () => {
-    console.log(`app is started on port: ${PORT}`);
-});
+const serverlessHandler = serverless(app); // create once
+
+export const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  await connectDB();
+
+  return serverlessHandler(event, context); // reuse handler
+};
